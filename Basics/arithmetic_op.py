afa06be16fbfaec67c7dc859f6134752dda9a5c7
BikramdeepSingh <biksaini20@gmail.com>
2022-02-02 18:40:15 +0530

    python basics

---
+++ Basics/arithmetic_op.py
@@ -0,0 +1,35 @@
+'''
+Arithmetic operations are just as same as that in mathematics
+Operator precedence
+
+   parenthesis
+   exponent
+   multiplication or division
+   addition or subtraction
+'''
+
+a=10
+b=5.5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) #returns float value 
+print(a//b) #retunrs rounded value
+print(a%b) #returns remainder of the division
+print(10**2) #return square of 10 
+
+x=1
+x += 3 #short form for x=x+3 or augmented assignment operator
+print(x)
+x -= 1
+print(x)
+x *= 2 
+print(x)
+x /= 2
+print(x)
+x //=2
+print(x)
+
+
+
---
+++ Basics/math_fun.py
@@ -0,0 +1,14 @@
+'''
+Math functions in python
+'''
+
+import math #it imports math module. now math is an object like a string
+# now we can access the methods of math using . operator
+
+x=2.9
+print(round(x)) #returns rounded value
+print(abs(-5)) #retuns absolute value which is always positive
+#for complex mathematical calculations
+print(math.ceil(2.1)) #returns upper rounded value i.e. 3
+print(math.floor(2.1))  #returns lower rounded value i.e. 2
+print(math.factorial(3)) #returns factorial of passed value and error if value is not an integer or negative
