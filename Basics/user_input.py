b249bfa90f02afb801631441e05eafb9c87198da
BikramdeepSingh <biksaini20@gmail.com>
2022-02-02 16:15:53 +0530

    Python basics

---
+++ Basics/user_input.py
@@ -0,0 +1,3 @@
+#input is an builtin function responsible for user input 
+name = input("Enter your name :") #storing user input in name variable
+print("Your name is",name)
---
+++ Basics/variables.py
@@ -0,0 +1,8 @@
+#Variables are named memory locations to store values 
+age=10  #integer variable
+name='Sam' #string variable
+price=55.5 #float variable
+Pass=True #Boolean variable
+
+print("My name is",name,"and I am", age,"years old") #print is a pre defined function, used to display the output
+
