4a876fe1d45789525c16ff8a92ee3815ee350883
BikramdeepSingh <biksaini20@gmail.com>
2022-02-02 18:52:57 +0530

    conditions and loops in python

---
+++ Conditions_and_Loops/conditions.py
@@ -0,0 +1,10 @@
+is_hot= False
+is_cold=True
+
+if is_hot: # executes if condition is True
+    print('Drink plenty of water')
+elif is_cold: # executes if 1st condition is False
+    print('Wear warm clothes')
+else: # executes if none of the condition is True
+    print('Neither too hot nor too cold')
+print('Enjoy your day')
