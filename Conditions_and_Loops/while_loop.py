96ff59a127262971c284ef62c400feb7aafb3ed6
BikramdeepSingh <biksaini20@gmail.com>
2022-02-03 18:38:31 +0530

    loops in python

---
+++ Conditions_and_Loops/while_loop.py
@@ -0,0 +1,14 @@
+'''
+While loops are used to execute a block of code multiple times.
+a condition is used to check whether to run the loop or not.
+'''
+
+i = 1
+while i<=6: #continues till the condition gets false
+    print(i)
+    i += 1 #increment 1 every time the loop is executed
+
+j=1
+while j<=5:
+    print('*' * j) #it is an expression to form a pattern where * will get repeated with value of j
+    j += 1
