3674888e9771f5f998d5e7ab8bf3e6ced77fc11e
BikramdeepSingh <biksaini20@gmail.com>
2022-02-07 19:42:11 +0530

    functions

--- Advance_topics/functions.py
+++ Advance_topics/functions.py
@@ -4,6 +4,8 @@ Functions
  Breaking the code smaller, managable and maintable chunks known as functions
  Functions contains lines of code that perform a specific task
  def is used to define a function
+ we can define a function with multiple arguments
+ Position of arguments matter otherwise the will be a bug in the code
  functions are called after defining them
 '''
 
@@ -12,10 +14,21 @@ Built in functions: already defined functions like input(), print()
 User defined functions: lines of code written by the coder
 '''
 
-def greetings(uname): #here greetings is fn name and uname is argument
-    print(f'''Hi {name}! 
+def greetings(fname,lname): #here greetings is fn name and uname is parameter
+    print(f'''Hi {fname} {lname}! 
 Welcome aboard''') #formatted string
 
-name=input('Please enter your name:')
-greetings(name)
+f_name=input('Please enter your first name:')
+l_name=input('Please enter your last name:')
+greetings(f_name,l_name) #it takes positional arguments
+greetings(lname='Kom', fname='Mary') #it takes keyword arguments
+'''
+Keyword arguments can be written in any order
+ It improves the readability of the code and can be used while working 
+ with numeric values.
+ Keyword arguments should always come after positional arguments.
+'''
 
+greetings('Sam', lname='Billings')
+# greetings('Billings', fname='Sam') It will give an error for multiple values for fname
+# greetings(fname='Sam', 'Billings') It will give an error for keyword argument before positional
