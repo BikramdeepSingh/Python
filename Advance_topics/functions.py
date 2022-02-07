d342c1c47e4fe3099232b1c73e1df68fc9c3a685
BikramdeepSingh <biksaini20@gmail.com>
2022-02-07 19:17:12 +0530

    functions in py

---
+++ Advance_topics/functions.py
@@ -0,0 +1,20 @@
+'''
+Functions
+functions are better way of organising the code
+Breaking the code smaller, managable and maintable chunks known as functions
+Functions contains lines of code that perform a specific task
+def is used to define a function
+'''
+
+'''
+Built in functions: already defined functions like input(), print()
+User defined functions: lines of code written by the coder
+'''
+
+def greetings():
+    print('''Hi there!
+Welcome aboard''')
+
+
+greetings()
+
