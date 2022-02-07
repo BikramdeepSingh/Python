5739d66ac880a36b72841a05dec2d980cca7d32e
BikramdeepSingh <biksaini20@gmail.com>
2022-02-07 19:50:51 +0530

    return statement

---
+++ Advance_topics/return.py
@@ -0,0 +1,14 @@
+'''
+Return Statement
+ It is used to return the result of a function to its call
+ If we dont have a return statement in a function, by default a function
+ returns None 
+'''
+
+def square(num):
+    return num*num
+
+
+n1=square(5)
+print(n1)
+print(square(3))
