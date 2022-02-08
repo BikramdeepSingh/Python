83580ff77ebf1ebdcab3ed3e1c6f182a3ae93eb8
BikramdeepSingh <biksaini20@gmail.com>
2022-02-08 13:53:15 +0530

    comments in py

---
+++ Basics/comments.py
@@ -0,0 +1,12 @@
+'''
+Comments are part of the code which an interpreter ignores.
+Comments can be used to explain Python code.
+Comments can be used to make the code more readable.
+Comments can be used to prevent execution when testing code.
+
+Single line comments are written with # in front of them
+Multiline comments take 'triple inverted commas in the geginning
+of the comment and at the end of the comment.
+
+
+'''
