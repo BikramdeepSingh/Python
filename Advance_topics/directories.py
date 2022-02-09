0d9fe1ec5c4f5567fb2a0c96bd11775d9cbd7af1
BikramdeepSingh <biksaini20@gmail.com>
2022-02-09 20:25:43 +0530

    directories in py

--- Advance_topics/directories.py
+++ Advance_topics/directories.py
@@ -10,6 +10,19 @@ eg: c:\Program Files\Microsoft
 Relative path: It works from the current directory
 '''
 path1=Path("Conditions_and_Loops") #if we don't pass an argument, it will refer to current directory
-path2=Path("disk1")
+path2=Path("test_directory")
 print(path1.exists()) #it returns a boolean value
 print(path2.exists()) 
+
+# path2.mkdir() # creates a new directory i.e. make directory
+# print('New test directory made:',path2.exists())
+
+# path2.rmdir()#it deletes a directory with the given name i.e. remove directory
+# print('New test directory:',path2.exists())
+
+path3=Path()#current directory
+
+for file in path3.glob('*'):
+    print(file) #prints every directory and files
+#we can optionally add an extension but with that we will only get files 
+#not the directories i.e. print(path3.glob('*.*'))
