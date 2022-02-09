b640d90d57253546b8c79989e114649a4a4a12d7
BikramdeepSingh <biksaini20@gmail.com>
2022-02-09 19:58:58 +0530

    dice flip in py

--- Practice_Programs/dice_flip.py
+++ Practice_Programs/dice_flip.py
@@ -5,7 +5,24 @@ Two dice are rolled and output is displayed on console
 import random
 
 
-dice_1= [1, 2, 3, 4, 5, 6]
-dice_2= [1, 2, 3, 4, 5, 6]
+class Dice:
+    def __init__(self):
+        pass
 
-print(f'({random.choice(dice_1)},{random.choice(dice_2)})')
+    def roll(self):
+        dice_1= (1, 2, 3, 4, 5, 6)
+        dice_2= (1, 2, 3, 4, 5, 6)
+        '''
+        Or 
+        first = random.randint(1,6)
+        second = random.randint(1,6)
+        
+        '''
+        first=random.choice(dice_1)
+        second=random.choice(dice_2)
+        return first,second #it returns a tuple of values
+
+
+
+dice_1 = Dice()
+print(dice_1.roll() )
