6d726f88860519b37d55bd36a8776227907a77e7
BikramdeepSingh <biksaini20@gmail.com>
2022-02-09 19:50:31 +0530

    dice flip program

---
+++ Practice_Programs/dice_flip.py
@@ -0,0 +1,11 @@
+'''
+Two dice are rolled and output is displayed on console
+'''
+
+import random
+
+
+dice_1= [1, 2, 3, 4, 5, 6]
+dice_2= [1, 2, 3, 4, 5, 6]
+
+print(f'({random.choice(dice_1)},{random.choice(dice_2)})')
