eb7d2b53e525ced5a789542f266ddbf7cc51e6f0
BikramdeepSingh <biksaini20@gmail.com>
2022-02-15 19:49:40 +0530

    writing to a file

---
+++ File_Handling/file_write_operations.py
@@ -0,0 +1,6 @@
+'''
+To write to an existing file, we must add a parameter to the open() function:
+"a" - Append - will append to the end of the file
+"w" - Write - will overwrite any existing content
+'''
+
