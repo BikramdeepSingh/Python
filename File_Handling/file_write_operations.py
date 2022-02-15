6a41e28c3fce4dae84d625e8483bb56bca46a4aa
BikramdeepSingh <biksaini20@gmail.com>
2022-02-15 19:51:11 +0530

    writing to a file

--- File_Handling/file_write_operations.py
+++ File_Handling/file_write_operations.py
@@ -4,3 +4,14 @@ To write to an existing file, we must add a parameter to the open() function:
 "w" - Write - will overwrite any existing content
 '''
 
+try:
+    f = open("D:\Python\File_Handling\sample_file.txt", "a") #it will open the file in read mode
+    f.write("This was written using append method")
+    
+except:
+    print('Error in accessing the file!')
+else: #if there is no exception else will work
+    print('Successful write')
+finally:
+    f.close()
+    print('Closed the file successfully')
