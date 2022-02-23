6768a96b0b0b81b31244f5dea4236843a9b8c8cc
BikramdeepSingh <biksaini20@gmail.com>
2022-02-23 15:17:37 +0530

    enumerate

--- Advance_topics/enumerate.py
+++ Advance_topics/enumerate.py
@@ -1,5 +1,7 @@
 ''' 
-Enumertae is used to iterate in a list
+Enumerate is used to iterate in a list
+It returns a tuple for each element in the list.
+1st value in tuple is the index of the value in the sequence and 2nd value is the element in the sequence
 It returns the item along with the index of that item.
 '''
 
@@ -8,3 +10,8 @@ l1 = ['Kelly', 'Sam', 'Tim', 'John']
 
 for name in enumerate(l1):
     print(name)
+
+l2 = ('Tom', 'Samules', 'Chadwick')
+
+for index,winner in enumerate(l2):
+    print('{} - {}'.format(index+1,winner)) 
