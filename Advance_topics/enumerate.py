dd06f3e7a3ff9cb7a5099c5042f50bafebc4a387
BikramdeepSingh <biksaini20@gmail.com>
2022-02-23 15:08:31 +0530

    enumerate in py

---
+++ Advance_topics/enumerate.py
@@ -0,0 +1,10 @@
+''' 
+Enumertae is used to iterate in a list
+It returns the item along with the index of that item.
+'''
+
+
+l1 = ['Kelly', 'Sam', 'Tim', 'John']
+
+for name in enumerate(l1):
+    print(name)
