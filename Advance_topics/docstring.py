ebe15e89ce1668b5496d9ca938a2eca21ad54ac7
BikramdeepSingh <biksaini20@gmail.com>
2022-02-25 11:23:22 +0530

    docstring in py

---
+++ Advance_topics/docstring.py
@@ -0,0 +1,6 @@
+'''
+A docstring is a brief text that explains what something does.
+We want our methods, classes, and functions to give us more information when we 
+or someone else use the help function. We can do that by adding a docstring.
+'''
+
