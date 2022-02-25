e4e0d0e635abf281bdba32f426eecd526bd2605c
BikramdeepSingh <biksaini20@gmail.com>
2022-02-25 11:26:42 +0530

    docstring use in help to gain insights of fn/class

--- Advance_topics/docstring.py
+++ Advance_topics/docstring.py
@@ -2,5 +2,10 @@
 A docstring is a brief text that explains what something does.
 We want our methods, classes, and functions to give us more information when we 
 or someone else use the help function. We can do that by adding a docstring.
+It is written in triple quotes notation
 '''
+
+def to_seconds(hours,minutes,seconds):
+    ''' This method converts hours, minutes and seconds to seconds'''
+    return hours*3600+minutes*60+seconds
 
